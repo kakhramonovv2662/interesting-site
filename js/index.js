3bb4147fed00602c7791895405234f58b7e2bb59
kakhramonovv2662 <qahramonovismoil860@gmail.com>
2022-02-04 16:48:55 +0500

    finished site

---
+++ js/index.js
@@ -0,0 +1,620 @@
+let elWrappper = document.querySelector(".js-wrapper");
+
+let elRed = document.querySelector(".js-red");
+let elYellow = document.querySelector(".js-yellow");
+let elGreen = document.querySelector(".js-green");
+
+let elReds = document.querySelector(".js-reds");
+let elYellows = document.querySelector(".js-yellows");
+let elGreens = document.querySelector(".js-greens");
+
+let iconImg = document.querySelector(".js-icon-img");
+
+let carImg = document.querySelector(".js-car-img");
+let carLeftImg = document.querySelector(".js-left-car");
+let carRightImg = document.querySelector(".js-right-img");
+let carRightSecondImg = document.querySelector(".js-right-icon-img");
+
+function carTimeOut() {
+  setTimeout(() => {
+    carImg.style.right = "50px";
+  }, 500);
+
+  setTimeout(() => {
+    carImg.style.right = "80px";
+  }, 1000);
+
+  setTimeout(() => {
+    carImg.style.right = "110px";
+  }, 1500);
+
+  setTimeout(() => {
+    carImg.style.right = "140px";
+  }, 2000);
+
+  setTimeout(() => {
+    carImg.style.right = "175px";
+  }, 2500);
+
+  setTimeout(() => {
+    carImg.style.right = "205px";
+  }, 3000);
+
+  setTimeout(() => {
+    carImg.style.right = "235px";
+  }, 3500);
+
+  setTimeout(() => {
+    carImg.style.right = "265px";
+  }, 4000);
+
+  setTimeout(() => {
+    carImg.style.right = "300px";
+  }, 4500);
+
+  setTimeout(() => {
+    carImg.style.right = "340px";
+  }, 15000);
+
+  setTimeout(() => {
+    carImg.style.right = "380px";
+  }, 15500);
+
+  setTimeout(() => {
+    carImg.style.right = "420px";
+  }, 16000);
+
+  setTimeout(() => {
+    carImg.style.right = "460px";
+  }, 16500);
+
+  setTimeout(() => {
+    carImg.style.right = "500px";
+  }, 17000);
+
+  setTimeout(() => {
+    carImg.style.right = "540px";
+  }, 17500);
+
+  setTimeout(() => {
+    carImg.style.right = "580px";
+  }, 18000);
+
+  setTimeout(() => {
+    carImg.style.right = "620px";
+  }, 18500);
+
+  setTimeout(() => {
+    carImg.style.right = "660px";
+  }, 19000);
+
+  setTimeout(() => {
+    carImg.style.right = "700px";
+  }, 19500);
+
+  setTimeout(() => {
+    carImg.style.right = "740px";
+  }, 20000);
+
+  setTimeout(() => {
+    carImg.style.right = "780px";
+  }, 20500);
+
+  setTimeout(() => {
+    carImg.style.right = "820px";
+  }, 21000);
+
+  setTimeout(() => {
+    carImg.style.right = "860px";
+  }, 21500);
+
+  setTimeout(() => {
+    carImg.style.right = "900px";
+  }, 22000);
+
+  setTimeout(() => {
+    carImg.style.right = "950px";
+  }, 22500);
+
+  setTimeout(() => {
+    carImg.style.right = "990px";
+  }, 23000);
+
+  setTimeout(() => {
+    carImg.style.right = "1400px";
+  }, 23500);
+}
+
+carTimeOut();
+
+function carLeftTimeOut() {
+  setTimeout(() => {
+    carLeftImg.style.right = "40px";
+  }, 14500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "80px";
+  }, 15000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "110px";
+  }, 15500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "140px";
+  }, 16000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "190px";
+  }, 16500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "230px";
+  }, 17000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "275px";
+  }, 17500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "305px";
+  }, 18000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "345px";
+  }, 18500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "380px";
+  }, 19000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "420px";
+  }, 19500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "460px";
+  }, 20000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "500px";
+  }, 20500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "540px";
+  }, 21000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "580px";
+  }, 21500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "620px";
+  }, 22000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "660px";
+  }, 22500);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "700px";
+  }, 23000);
+
+  setTimeout(() => {
+    carLeftImg.style.right = "750px";
+  }, 23500);
+}
+
+carLeftTimeOut();
+
+function carRightTimeOut() {
+  setTimeout(() => {
+    carRightImg.style.left = "40px";
+  }, 500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "80px";
+  }, 1000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "110px";
+  }, 1500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "140px";
+  }, 2000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "190px";
+  }, 2500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "230px";
+  }, 3000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "275px";
+  }, 3500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "305px";
+  }, 4000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "345px";
+  }, 12000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "380px";
+  }, 12500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "420px";
+  }, 13000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "460px";
+  }, 13500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "500px";
+  }, 14000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "540px";
+  }, 14500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "580px";
+  }, 15000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "620px";
+  }, 15500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "660px";
+  }, 16000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "700px";
+  }, 16500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "750px";
+  }, 17000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "780px";
+  }, 17500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "825px";
+  }, 18000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "865px";
+  }, 18500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "910px";
+  }, 19000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "950px";
+  }, 19500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1000px";
+  }, 20000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1040px";
+  }, 20500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1090px";
+  }, 21000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1140px";
+  }, 21500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1190px";
+  }, 22000);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1240px";
+  }, 22500);
+
+  setTimeout(() => {
+    carRightImg.style.left = "1280px";
+  }, 23000);
+}
+
+carRightTimeOut();
+
+function carRightSecond() {
+  setTimeout(() => {
+    carRightSecondImg.style.left = "40px";
+  }, 14500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "80px";
+  }, 15000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "110px";
+  }, 15500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "140px";
+  }, 16000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "190px";
+  }, 16500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "230px";
+  }, 17000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "275px";
+  }, 17500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "305px";
+  }, 18000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "345px";
+  }, 18500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "380px";
+  }, 19000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "420px";
+  }, 19500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "460px";
+  }, 20000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "500px";
+  }, 20500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "540px";
+  }, 21000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "580px";
+  }, 21500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "620px";
+  }, 22000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "660px";
+  }, 22500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "700px";
+  }, 23000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "750px";
+  }, 23500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "780px";
+  }, 24000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "840px";
+  }, 24500);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "880px";
+  }, 25000);
+
+  setTimeout(() => {
+    carRightSecondImg.style.left = "930px";
+  }, 25500);
+}
+
+carRightSecond();
+
+function setTimeOut() {
+  setTimeout(() => {
+    elRed.style.backgroundColor = "red";
+  }, 500);
+
+  setTimeout(() => {
+    elYellow.style.backgroundColor = "yellow";
+  }, 10000);
+
+  setTimeout(() => {
+    elYellow.style.backgroundColor = "rgb(107, 92, 92)";
+  }, 12500);
+
+  setTimeout(() => {
+    elYellow.style.backgroundColor = "yellow";
+  }, 13000);
+
+  setTimeout(() => {
+    elRed.style.backgroundColor = "rgb(107, 92, 92)";
+    elYellow.style.backgroundColor = "rgb(107, 92, 92)";
+  }, 14000);
+
+  setTimeout(() => {
+    elGreen.style.backgroundColor = "green";
+  }, 14500);
+}
+
+setTimeOut();
+
+function setSecondTimeOut() {
+  setTimeout(() => {
+    elReds.style.backgroundColor = "red";
+  }, 500);
+
+  setTimeout(() => {
+    elYellows.style.backgroundColor = "yellow";
+  }, 10000);
+
+  setTimeout(() => {
+    elYellows.style.backgroundColor = "rgb(107, 92, 92)";
+  }, 12500);
+
+  setTimeout(() => {
+    elYellows.style.backgroundColor = "yellow";
+  }, 13000);
+
+  setTimeout(() => {
+    elReds.style.backgroundColor = "rgb(107, 92, 92)";
+    elYellows.style.backgroundColor = "rgb(107, 92, 92)";
+  }, 14000);
+
+  setTimeout(() => {
+    elGreens.style.backgroundColor = "green";
+  }, 14500);
+}
+
+setSecondTimeOut();
+
+function peoples() {
+  setTimeout(() => {
+    iconImg.style.marginTop = "20px";
+  }, 500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "35px";
+  }, 1000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "45px";
+  }, 1500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "60px";
+  }, 2000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "75px";
+  }, 2500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "90px";
+  }, 3000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "105px";
+  }, 3500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "120px";
+  }, 4000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "135px";
+  }, 4500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "150px";
+  }, 5000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "165px";
+  }, 5500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "180px";
+  }, 6000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "195px";
+  }, 6500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "210px";
+  }, 7000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "225px";
+  }, 7500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "240px";
+  }, 8000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "255px";
+  }, 8500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "270px";
+  }, 9000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "290px";
+  }, 9500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "310px";
+  }, 10000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "329px";
+  }, 10500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "345px";
+  }, 11000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "360px";
+  }, 11500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "375px";
+  }, 12000);
+  setTimeout(() => {
+    iconImg.style.marginTop = "385px";
+  }, 12500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "400px";
+  }, 13000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "420px";
+  }, 13500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "435px";
+  }, 14000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "450px";
+  }, 14500);
+  setTimeout(() => {
+    iconImg.style.marginTop = "470px";
+  }, 15000);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "500px";
+  }, 15500);
+
+  setTimeout(() => {
+    iconImg.style.marginTop = "520px";
+  }, 16000);
+}
+
+peoples();
